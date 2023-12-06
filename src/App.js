0b322117c4f2e68be6ca2a10d79ecdce2df1f5a9
Kanishka <Kanishkadeo92022@gmail.com>
2023-12-06 12:22:32 +0530

    commit after lecture 16

--- src/App.js
+++ src/App.js
@@ -1,20 +1,76 @@
 import './App.css';
-import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import  {News}  from './components/News';
+import About from './components/About';
+import React , { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router, 
+  Switch,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
+
 
 function App() {
+  const [mode , setMode] = useState('light');//state variable which shows whether dark mode is enabled or not
+  const [alert , setAlert] = useState(null);//alert ek object h , set alert ek function hai jo alert messgae ko set krega
+  
+  const toggleMode = ()=>{
+    if(mode === 'light')
+    {
+        setMode('dark');
+      document.body.style.backgroundColor = '#043070';
+      showAlert("Dark mode has been enabled","success");
+      document.title = "TextUtils-Dark Mode";
+      /*setInterval(()=>{
+        document.title = "TextUtils is Amazing";
+      },2000);
+      setInterval(()=>{
+        document.title = " Install TextUtils now";
+      },1500);*/
+    }
+    else
+    { 
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled","success");
+      document.title = "TextUtils-Light Mode";
+    }
+  }
+
+  const showAlert = (message,type)=>{ //it is a function to show alert msg on our screen
+    setAlert({
+      msg:message,
+      type:type
+    })//pehle alert null tha ab alert ek object hai
+    setTimeout(()=>{
+      setAlert(null);
+    },1500);
+  }
+
   return (
     <>
+    <Router>
       {/* <Navbar title = "TextUtils" aboutText ="About- TextUtils"/> */}
       {/* <Navbar/> */}
-        <Navbar title = "TextUtils"/>
+        <Navbar title = "TextUtils" mode = {mode} toggleMode = {toggleMode}/>
+        <Alert alert={alert}/>
         <div className='container my-3'>
-          <About/>
-          <TextForm heading="Enter the text to analyze below"/>
-        </div> 
-          {/* <News/> */}
+        <Routes>
+            {/* agar exact use nhi kia toh dono likhne pr component 1 hi render ho jaega 
+            islyee exact likhna zruri h
+            /users --> Component 1
+            /users/home --> omponent 2 */}
+            <Route exact path="/about" element={<About />}/> 
+            <Route exact path="/" element={<TextForm showAlert = {showAlert} heading="Enter the text to analyze below" mode = {mode} toggleMode = {toggleMode}/> }/>  
+        </Routes>
+        </div>  
+    
+    </Router>
+
+  
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,33 +6,40 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         // setText("you have clicked on handleUpClick")
         setText(newText)
+        props.showAlert("Converted to uppercase","success");
     }
     const handleLoClick = ()=>{
         // console.log('uppercase was clicked'+text)
         let newText = text.toLowerCase();
         // setText("you have clicked on handleUpClick")
         setText(newText)
+        props.showAlert("Converted to lowercase","success");
     }
     const handleSummClick = ()=>{
         let newText = text.slice(0,200);
-        setText(newText)
+        setText(newText);
+        props.showAlert("Text Summary Generated","success");
     }
     const handleClearClick = ()=>{
         let newText = " ";
-        setText(newText)
+        setText(newText);
+        props.showAlert("Cleared text","success");
     }
     const handleLineClick = ()=>{
         let newText = text.trimEnd();
-        setText(newText)
+        setText(newText);
+        props.showAlert("Text Trimmed","success");
     }
     const handleCopy = ()=>{
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.showAlert("Copied to clipboard","success");
     }
     const handleExtraSpaces = ()=>{
         let newText = text.split(/[ ]+/);
-        setText(newText.join(" "))
+        setText(newText.join(" "));
+        props.showAlert("Extra Spaces Removed","success");
     }
     
     const handleOnChange = (event)=>{
@@ -46,11 +53,11 @@ export default function TextForm(props) {
     // setText("new text");//correct way to change the state
   return (
     <>
-    <div className='container'>
-        <h1>{props.heading}</h1>
+    <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
+        <h1 >{props.heading}</h1>
         <div className="mb-3">
         {/* <label for="myBox" className="form-label">{props.heading}</label> */}
-        <textarea className="form-control" value = {text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value = {text} onChange={handleOnChange} style={{backgroundColor:props.mode==='light'?'white':'#043070' , color:props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
@@ -59,12 +66,12 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-2" onClick={handleCopy}>copy Text</button>
         <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
       <h2>Your text summary</h2>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p>{0.008 * text.split(" ").length} time taken to read the text</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length>0?text:"Enter something in the textbox above to preview it here!!"}</p>
     </div>
     </>
   )
